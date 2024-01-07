155b863e63313d994ce290938ce0a31d07370598
fahadtahir02 <fahad.tahir79@myhunter.cuny.edu>
2024-01-06 22:35:08 -0500

    ,l

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -7,5 +7,5 @@ from .views import RoomView
 
 #All main urls endpoints will be recieved here first then routed where they should be. This is the main hub per say. 
 urlpatterns = [
-    path('home', RoomView.as_view()),
+    path('room', RoomView.as_view()),
 ]
